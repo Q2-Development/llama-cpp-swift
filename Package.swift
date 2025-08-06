406b33c28455041b0abbac74289b3c3bb3182014
Michaelgathara <mikegtrm@gmail.com>
2025-08-06 01:05:23 -0500

    feat: new version

--- Package.swift
+++ Package.swift
@@ -16,16 +16,20 @@ let package = Package(
             targets: ["LLamaSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ggerganov/llama.cpp", branch: "master"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.1"),
     ],
     targets: [
         .target(
-            name: "LLamaSwift",        
+            name: "LLamaSwift",
             dependencies: [
-                .product(name: "llama", package: "llama.cpp"),
+                "LlamaFramework",
                 .product(name: "Logging", package: "swift-log"),
             ]
+        ),
+        .binaryTarget(
+            name: "LlamaFramework",
+            url: "https://github.com/ggml-org/llama.cpp/releases/download/b6097/llama-b6097-bin-win-opencl-adreno-arm64.zip",
+            checksum: "3c2689d7e4a321217b775d4310a6b823dfe4f67d68ee5566e79bf58c7eab2ab8"
         ),
         .testTarget(
             name: "llama-cpp-swiftTests",
